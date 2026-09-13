855cba7a34ae7e889266b1bd853d404ee7c62cef
Venusepikisverycool <valentinomarsic@Valentinos-Air.lan>
2026-09-13 16:39:53 -0400

    Alpha 0.1.0

---
+++ src/client/java/dev/toomanyachievements/tma/client/ToomanyachievementsClient.java
@@ -0,0 +1,10 @@
+package dev.toomanyachievements.tma.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToomanyachievementsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/toomanyachievements/tma/Toomanyachievements.java
@@ -0,0 +1,10 @@
+package dev.toomanyachievements.tma;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Toomanyachievements implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
